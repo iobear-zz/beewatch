a9e3292c761353b0da07404911e9981e361a970d
iobear <beewatch@bjornz.dk>
2012-10-10 12:44:10 +0200

    moved some JS to jquery

--- www/mouseOver.js
+++ www/mouseOver.js
@@ -1,39 +1,39 @@
-//Mouse related functions
+//Mouse related functions 
 
 document.write( '<br /><br /><div class="kasse">');
 document.write( '<div id="macAddr" class="headline">MAC </div>' );
-document.write( '<div id="url"></div>' );
-document.write( '<div id="tstamp"></div>' );
-document.write( '<div id="upt"></div>' );
+document.write( '<div class="url"></div>' );
+document.write( '<div class="tstamp"></div>' );
+document.write( '<div class="upt"></div>' );
 document.write( '</div><div class="kasse">');
 document.write( '<div class="headline">Decoder</div>' );
 
-document.write( '<div id="ddecodeDrops"></div>' );
-document.write( '<div id="decodeErr"></div>' );
-document.write( '<div id="decodeOflow"></div>' );
+document.write( '<div class="ddecodeDrops"></div>' );
+document.write( '<div class="decodeErr"></div>' );
+document.write( '<div class="decodeOflow"></div>' );
 document.write( '</div><div class="kasse">');
 document.write( '<div class="headline">Display</div>' );
-document.write( '<div id="displayDrops"></div>' );
-document.write( '<div id="displayErr"></div>' );
-document.write( '<div id="displayOflow"></div>' );
+document.write( '<div class="displayDrops"></div>' );
+document.write( '<div class="displayErr"></div>' );
+document.write( '<div class="displayOflow"></div>' );
 document.write( '</div><div class="kasse">');
 document.write( '<div class="headline">PTS</div>' );
-document.write( '<div id="ptsDrops"></div>' );
-document.write( '<div id="ptsOflow"></div>' );
+document.write( '<div class="ptsDrops"></div>' );
+document.write( '<div class="ptsOflow"></div>' );
 
 document.write( '</div><div class="kassesidst">');
 document.write( '<div class="headline">Misc</div>' );
-document.write( '<div id="iframeErr"></div>' );
-document.write( '<div id="stalled"></div>' );
-document.write( '<div id="badStream"></div>' );
-document.write( '<div id="operaCrash"></div>' );
+document.write( '<div class="iframeErr"></div>' );
+document.write( '<div class="stalled"></div>' );
+document.write( '<div class="badStream"></div>' );
+document.write( '<div class="operaCrash"></div>' );
 document.write( '</div>');
 
  $(document).ready(function(){
 	$("div.st").mouseenter(function(){
 		var timeMac = $(this).attr('id').split("_",2);
-		document.getElementById("tstamp").innerHTML = "time "+timeConverter(timeMac[0]);
-		document.getElementById("macAddr").innerHTML = "mac "+timeMac[1];
+		$(".tstamp").html("time "+timeConverter(timeMac[0]));
+		$(".macAddr").html("mac "+timeMac[1]);
 		pullRedis(getRedisUrl(timeMac[0], timeMac[1]), 'min');
 	});
 	$("div.st").mousedown(function(event){
@@ -45,40 +45,16 @@ document.write( '</div>');
 	});
 });
 
-
 function showDetails(){
 	var lasttime = 0;
 	var uptime = 0;
 	var lastErr;
-	$.each(data, function(index, value) {
-		upt = data[index]['upt'];
-		document.getElementById("upt").innerHTML = "uptime: "+formatUptime (upt);
-		url = returnChannel(data[index]['url']);
-		document.getElementById("url").innerHTML = "url: "+url;
-		badStream = data[index]['badStream'];
-		document.getElementById("badStream").innerHTML = "badStream: "+badStream;
-		ddecodeDrops = data[index]['ddecodeDrops'];
-		document.getElementById("ddecodeDrops").innerHTML = "ddecodeDrops: "+ddecodeDrops;
-		decodeErr = data[index]['decodeErr'];
-		document.getElementById("decodeErr").innerHTML = "decodeErr: "+decodeErr;
-		decodeOflow = data[index]['decodeOflow'];
-		document.getElementById("decodeOflow").innerHTML = "decodeOflow: "+decodeOflow;
-		displayDrops = data[index]['displayDrops'];
-		document.getElementById("displayDrops").innerHTML = "displayDrops: "+displayDrops;
-		displayErr = data[index]['displayErr'];
-		document.getElementById("displayErr").innerHTML = "displayErr: "+displayErr;
-		displayOflow = data[index]['displayOflow'];
-		document.getElementById("displayOflow").innerHTML = "displayOflow: "+displayOflow;
-		iframeErr = data[index]['iframeErr'];
-		document.getElementById("iframeErr").innerHTML = "iframeErr: "+iframeErr;
-		ptsDrops = data[index]['ptsDrops'];
-		document.getElementById("ptsDrops").innerHTML = "ptsDrops: "+ptsDrops;
-		ptsOflow = data[index]['ptsOflow'];
-		document.getElementById("ptsOflow").innerHTML = "ptsOflow: "+ptsOflow;
-		stalled = data[index]['stalled'];
-		document.getElementById("stalled").innerHTML = "stalled: "+stalled;
-		operaCrash = data[index]['operaCrash'];
-		document.getElementById("operaCrash").innerHTML = "operaCrash: "+operaCrash;
-
+	$.each(data, function(index, JSONvalue) {
+		$.each(JSONvalue, function(name, val) {
+			if (name == "upt") {
+				val = formatUptime(val);
+			}
+			$("."+name).html(name + ': ' + val);
+		});
 	});
 }
