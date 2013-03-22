74fa966a6ad67e1a2b86e16d755df5ea450b5923
iobear <beewatch@bjornz.dk>
2013-03-22 18:25:10 +0100

    Added EN metatag to avoid Irish lang detection by chrome

--- www/index2.php
+++ www/index2.php
@@ -2,6 +2,7 @@
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
+		<meta http-equiv="Content-Language" content="en" />
 		<title>Beewatch</title>
 		<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
 		<link href="css/bootstrap-responsive.css" rel="stylesheet" media="screen">
