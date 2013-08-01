ac04c089add4198337d93281ee878a095ecb40a9
Bjorn Jorgensen <bnj@waoo.dk>
2013-08-01 23:18:12 +0200

    Made an API for error counters

--- bin/parse_airties_logs.py
+++ bin/parse_airties_logs.py
@@ -31,20 +31,35 @@ def matchmyregex(line):
 
             datetimeUnix = str((int(datetimeUnix)+120)/300*300)  # round to strict 5 min interval
             dateMac = datetimeUnix + macNoDelimt
+            dateMacPrev = str(int(datetimeUnix) - 300) + macNoDelimt
+            dateMacPrevPrev = str(int(datetimeUnix) - 600) + macNoDelimt
+
             r_server.hset(dateMac, "stime", datetimeUnix)
             dateMacLog = 'log' + dateMac
 
-            r_server.sadd('who' + datetimeUnix, macNoDelimt)  # adds to list af MACs active within this 5 min window
+            # adds to list af MACs active within this 5 min window
+            r_server.sadd('who' + datetimeUnix, macNoDelimt)
             r_server.expire('who' + datetimeUnix, expireParsedLog)
 
             r_server_raw.hset(dateMacLog, lineNo, line)  # adds raw log to redis
             r_server_raw.expire(dateMacLog, expireRAWlogs)
 
+            #makes translation list for IP <-> MAC
             ip = line.split(' ')[1]
             r_server.hset(dateMac, "ip", ip)
             ipAton = reduce(lambda x, y: (x << 8) + y, [int(x) for x in ip.split('.')])
             r_server.set(ipAton, macNoDelimt)
             r_server.expire(ipAton, expireIPtoMAC)
+
+            #making a list of /16 IP ranges
+            ipSplit = ip.split('.')
+            IPstore16 = str(datetimeUnix) + ipSplit[0] + ipSplit[1]
+            IPstore16str = 'IP16' + IPstore16
+#            IPstoreStr24 = IPstore16str + ipSplit[2]
+#            r_server.sadd('IP24' + IPstoreStr24, macNoDelimt + '_0')  # _0 = no mcast error
+#            r_server.expire('IP24' + IPstoreStr24, expireRAWlogs)
+#            making a list of /16 IP ranges with active /24 ranges
+#            r_server.sadd('IP16' + IPstore16str, IPstoreStr24)
 
             if REGM_ipaddress.search(line):
                 firmware = REGEXip.findall(line)
@@ -78,6 +93,9 @@ def matchmyregex(line):
                     r_server.expire(dateMac, expireParsedLog)
 
             elif REGM_playing.search(line):
+                #counting boxes online in that iprange
+                r_server.hincrby(IPstore16str, 'totalOnline', 1)
+                r_server.expire(IPstore16str, expireRAWlogs)
                 if REGEXplayurl.search(line):
                     playurl = REGEXplayurl.findall(line)
                     r_server.hset(dateMac, "url", playurl[0].split('/')[2])
@@ -109,6 +127,25 @@ def matchmyregex(line):
                 r_server.hset(dateMac, "ddecodeDrops", dErrDrops[1])
                 r_server.hset(dateMac, "decodeErr", dErrErrors[1:-1])
                 r_server.expire(dateMac, expireParsedLog)
+                dErrNow = int(dErrErrors[1:-1])
+                if dErrNow:
+                    dErrLast = r_server.hget(dateMacPrev, "decodeErr")
+                    if dErrLast:
+                        dErrLast = int(dErrLast)
+                    elif dErrLast is None:
+                        dErrLast = r_server.hget(dateMacPrevPrev, "decodeErr")
+                        if dErrLast:
+                            dErrLast = int(dErrLast)
+                    else:
+                        print '0'
+                        dErrLast = 0
+
+                    if dErrNow > dErrLast:
+                        r_server.hincrby(IPstore16str, 'decodeErr', 1)
+                        if dErrLast is not None:
+                            dErrDiff = dErrNow - dErrLast
+                            if dErrDiff > 7:
+                                r_server.hincrby(IPstore16str, 'decodeErr8', 1)
 
             elif REGM_display.search(line):
                 displayErr = REGEXdecodeerr.findall(line)
@@ -143,11 +180,15 @@ def matchmyregex(line):
                 rtspErrLog = line.split("{")[1].split("}")[0]
                 r_server.hset(dateMac, "rtsperr", rtspErrLog)
                 r_server.expire(dateMac, expireParsedLog)
+                r_server.hincrby(IPstore16str, 'rtspErr', 1)
 
             elif REGM_mcast.search(line):
                 r_server.hset(dateMac, "mcast", 1)
                 r_server.hset(dateMac, "mac", macNoDelimt)
                 r_server.expire(dateMac, expireParsedLog)
+                #count mcast errors
+                r_server.hincrby(IPstore16str, 'mcastErr', 1)
+#                r_server.sadd(IPstoreStr24, macNoDelimt + '_1')  # _1 = mcast error
 
             elif REGM_invalid.search(line):
                 r_server.hset(dateMac, "invaliddata", 1)
---
+++ bin/statsApi.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+#
+
+from bottle import route, run, request, abort
+import redis
+import time
+import json
+
+r_server = redis.Redis(host='localhost', port=6380, db=0)
+
+IPranges = ['1037', '1038', '1039', '1044', '1045', '1049', '1050', '1051', '1056', '1061', '10221', '10230', '7768', '87104', '17228']
+
+
+@route('/v1/stats/err/:type/:callback', method='GET')
+def getDecodeErr(type, callback):
+    errPrBoxArr = []
+    htmlErrArr = {}
+
+    searchString = 'IP16' + getCurretTime()
+
+    for key in IPranges:
+        decodeErr = r_server.hget(searchString + key, type)
+        totalOnline = r_server.hget(searchString + key, 'totalOnline')
+
+        if decodeErr:
+            errPrBox = round(float(decodeErr)/float(totalOnline), 4)
+            errPrBox = str(round(errPrBox, 3))
+            errPrBoxArr.append(errPrBox)
+        else:
+            errPrBoxArr.append('0')
+
+    i = 0
+    for i, elem in enumerate(errPrBoxArr):
+        IPrange = IPranges[i]
+        htmlErrArr[str(IPrange)] = str(elem)
+        i += 1
+
+    return jsonp(callback, dict(htmlErrArr))
+
+
+@route('/status')
+def status():
+    return {'status': 'online', 'servertime': time.time()}
+
+
+def getCurretTime():
+    timeNow = time.time()
+    datetimeUnix = str((int(timeNow)+120)/300*300)  # round to strict 5 min interval
+    return str(int(datetimeUnix) - 300)  # search the previous 5 min interval
+
+
+def jsonp(request, dictionary):
+    return "%s(%s)" % (request, dictionary)
+    return dictionary
+
+run(host='88.83.68.98', port=5124, reloader=True)
